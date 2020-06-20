a025ba4beb47960290d16adcc7000e70e2f17795
zfh <fuhongzuo@gmail.com>
2020-06-20 15:15:19 +0800

    struct的使用

--- syntax/struct_test.go
+++ syntax/struct_test.go
@@ -1,8 +1,6 @@
 package syntax
 
 import (
-	"container/list"
-	"encoding/json"
 	"fmt"
 	"testing"
 )
@@ -94,74 +92,8 @@ func (m NewInt) isZero() bool {
 }
 
 // 为基本类型添加方法
-func TestBasicTypeMethod(t *testing.T) {
+func Test_BasicTypeMethod(t *testing.T) {
 	var i NewInt
 	// 默认值是0
 	fmt.Println(i.isZero())
-}
-
-// 指定字段的tag，实现json字符串的首字母小写
-type Person struct {
-	Name   string `json:"name"`
-	Age    int    `json:"-"`
-	Wechat string `json:"wechat,omitempty"`
-}
-
-// 将结构体序列化为 JSON
-func TestStructMarshal(t *testing.T) {
-	/*
-			json.Marshal() JSON输出的时候必须注意：
-		      1）首字母为小写时，为private字段，不会转换
-		      2）tag中带有自定义名称，那么这个自定义名称会出现在JSON的字段名中
-		      3）字段的tag是"-"，那么这个字段不会输出到JSON
-		      4）tag中如果带有"omitempty"选项，那么如果该字段值为空，就不会输出到JSON串中，
-		         比如 false、0、nil、长度为 0 的 array，map，slice，string
-		      5）如果字段类型是bool, string, int, int64等，而tag中带有",string"选项，
-		         那么这个字段在输出到JSON的时候会把该字段对应的值转换成JSON字符串
-	*/
-	person := Person{"dazuo", 1, ""}
-	data, _ := json.Marshal(person)
-	fmt.Println(string(data))
-}
-
-// JSON 数据转换成 Go 类型的值（Decode）
-func TestStructUnmarshal(t *testing.T) {
-	data := []byte(`{"name":"dazuo","age":22}`)
-	person := new(Person)
-	err := json.Unmarshal(data, &person)
-	if err != nil {
-		_ = fmt.Errorf("Can not decode data: %v\n", err)
-	}
-	fmt.Printf("%v\n, 类型 = %T\n", *person, person)
-}
-
-func Test_Map_marshal(t *testing.T) {
-	data := make(map[string]string, 0)
-	data["name"] = "dazuo"
-	data["age"] = "24"
-	bytes, _ := json.Marshal(data)
-	fmt.Println(string(bytes))
-
-	data2 := make(map[string]string, 0)
-	_ = json.Unmarshal(bytes, &data2)
-	fmt.Println(data2)
-}
-
-func Test_List_marshal(t *testing.T) {
-	tmpList := list.New()
-	tmpList.PushFront("dazuo")
-	tmpList.PushFront("age")
-
-	bytes, _ := json.Marshal(tmpList)
-	fmt.Println(string(bytes)) // list无法序列化
-}
-
-func Test_array(t *testing.T) {
-	tmpArr := []string{"dazuo", "age"}
-	bytes, _ := json.Marshal(tmpArr)
-	fmt.Println(string(bytes))
-
-	tmpArr2 := make([]string, 0, 0)
-	_ = json.Unmarshal(bytes, &tmpArr2)
-	fmt.Println(tmpArr2)
 }
