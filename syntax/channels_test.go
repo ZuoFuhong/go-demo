9fd8efeace7c60c54e5a2124430608d1046347db
zfh <fuhongzuo@gmail.com>
2020-06-20 16:46:31 +0800

    通道阻塞和通道关闭

--- syntax/channels_test.go
+++ syntax/channels_test.go
@@ -2,6 +2,7 @@ package syntax
 
 import (
 	"fmt"
+	"math/rand"
 	"strconv"
 	"testing"
 	"time"
@@ -14,6 +15,9 @@ func Test_Chan(t *testing.T) {
 
 	// 创建无缓冲的通道，通道存放的元素的类型为string
 	ch := make(chan string)
+
+	// channel默认上是阻塞的，也就是说，如果Channel满了，就阻塞写，如果Channel空了，就阻塞读。
+	//
 	// 创建一个有 10 个缓冲的通道，通道存放元素的类型为 string
 	// 通道分为无缓冲的通道和有缓冲的通道， Go 提供内置函数 len 和 cap，无缓冲的通道的len和cap都是 0，有缓冲的通道的
 	// len 代表没有被读取的元素数，cap 代表整个通道的容量。无缓冲的通道既可以用于通信，也可以用于两个 goroutine 的同步，
@@ -38,31 +42,31 @@ func Test_Chan(t *testing.T) {
 	close(ch)
 }
 
-// 通道可以分为两个方向，一个是读，另一个是写，假如一个函数的输入参数和输出参数都是相同的 chan 类型，则该函数可以调用自己，
-// 最终形成一个调用链。当然多个具有相同参数类 型的函数也能组成一个调用链，这很像 UNIX 系统的管道，是一个有类型的管道。
-func Test_chan(t *testing.T) {
-	in := make(chan int)
-	go func() {
-		for i := 0; i < 10; i++ {
-			in <- i
-		}
-		// 关闭通道表示将不再在该通道上发送任何值。
-		close(in)
-	}()
+// Channel的关闭
+// 关闭Channel可以通知对方内容发送完了，不用再等了
+func Test_close_chan(t *testing.T) {
+	channel := make(chan string)
+	rand.Seed(time.Now().Unix())
 
-	// 连读调用3次chain，相当于把in中的每个元素都加3
-	out := chain(chain(chain(in)))
-	for v := range out {
-		fmt.Printf("v = %d\n", v)
+	// 向channel发送随机个数的message
+	go func() {
+		cnt := rand.Intn(10)
+		for i := 0; i < cnt; i++ {
+			channel <- fmt.Sprintf("message-%2d", i)
+		}
+		close(channel) // 关闭Channel
+	}()
+	var more = true
+	var msg string
+	for more {
+		select {
+		// channel会返回两个值，一个是内容，一个是还有没有内容
+		case msg, more = <-channel:
+			if more {
+				fmt.Println(msg)
+			} else {
+				fmt.Println("channel closed!")
+			}
+		}
 	}
-}
-
-func chain(in chan int) chan int {
-	out := make(chan int)
-	go func() {
-		for v := range in {
-			out <- 1 + v
-		}
-	}()
-	return out
 }
