bded394688fd04d35fb740a9edbe56604b3ed6b3
zfh <fuhongzuo@gmail.com>
2020-08-23 21:50:37 +0800

    跨域资源共享 CORS

--- example/http/cors.go
+++ example/http/cors.go
@@ -2,28 +2,33 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/cms/user/login", func(w http.ResponseWriter, r *http.Request) {
-		//uri:  /cms/user/login , Methods:  OPTIONS		// 浏览器跨域，预检请求
-		//uri:  /cms/user/login , Methods:  POST
-		fmt.Println("uri: ", r.RequestURI, ", Methods: ", r.Method)
+/**
+ * 跨域资源共享 CORS
+ * Blog: https://www.cnblogs.com/maxzuo/p/13550930.html
+ */
+func HTTPCORS() {
+	http.HandleFunc("/index", func(w http.ResponseWriter, r *http.Request) {
 		header := w.Header()
 		header.Set("Access-Control-Allow-Origin", "*")
-		header.Set("Access-Control-Allow-Headers", "*")
-		header.Set("Access-Control-Allow-Credentials", "true")
+		header.Set("Access-Control-Allow-Headers", "x-Custome-Header,Content-Type")
+		header.Set("Access-Control-Allow-Credentials", "false")
 		header.Set("Access-Control-Allow-Methods", "POST,GET,DELETE,PUT,OPTIONS")
-		header.Set("Content-Type", "application/json;charset=utf-8")
+		header.Set("Access-Control-Max-Age", "5")
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusNoContent)
-			return
 		}
-		_, _ = w.Write([]byte("{\"name\": \"dazuo\"}"))
+		reqBody, _ := ioutil.ReadAll(r.Body)
+		fmt.Printf("RequestURI: %s, Methods: %s, ReqBody: %s\n", r.RequestURI, r.Method, reqBody)
+		_, _ = w.Write([]byte("welcome !"))
 	})
-	err := http.ListenAndServe("127.0.0.1:8081", nil)
+
+	err := http.ListenAndServe("127.0.0.1:8080", nil)
 	if err != nil {
-		panic(err)
+		log.Panic(err)
 	}
 }
