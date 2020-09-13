318982f06525af45a27bc6e7eb05c5e9c381f8c7
zfh <fuhongzuo@gmail.com>
2020-09-13 12:23:50 +0800

    glog日志库的使用

--- third-tool/glog/glog.go
+++ third-tool/glog/glog.go
@@ -6,15 +6,32 @@ package main
 
 import (
 	"flag"
+
 	"github.com/golang/glog"
 )
 
-// 使用示例：go run glog.go -log_dir="./" -v=3 -alsologtostderr=true -stderrthreshold=-1 -logtostderr=true
-//  -log_dir：指定log files的目录，默认是os.TempDir()
-//  -v：是vLog是用户自定义的log级别
-//  -alsologtostderr：同时输出到os.Stderr和log files
-//  -stderrthreshold：大于等于该severity的log，会输出到os.Stderr
-//  -logtostderr：只输出到os.Stderr，
+/*
+	一、介绍：
+		golang/glog 是 C++ 版本 google/glog 的 Go 版本实现，基本实现了原生 glog 的日志格式。在 Kuberntes 中，glog 是默认日志库。
+
+    二、日志级别
+		INFO：普通日志；
+		WARNING：告警日志；
+		ERROR：错误日志；
+		FATAL：严重错误日志，打印完日志后程序将会推出（os.Exit()）
+
+	三、使用示例：
+		$ go run glog.go -log_dir="./" -v=3 -alsologtostderr=true -stderrthreshold=-1 -logtostderr=true
+	 	其中：
+		-log_dir：指定log files的目录，默认是os.TempDir()
+		-v：是vLog是用户自定义的log级别
+		-alsologtostderr：同时输出到os.Stderr和log files
+		-stderrthreshold：大于等于该severity的log，会输出到os.Stderr
+		-logtostderr：只输出到os.Stderr
+
+	四、参考资料
+		Golang glog使用详解：https://www.cnblogs.com/sunsky303/p/11081165.html
+*/
 func main() {
 	flag.Parse()
 	defer glog.Flush()
