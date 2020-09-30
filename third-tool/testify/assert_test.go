886545778f0fad65e619e2795bbee33826c2d3f8
zfh <fuhongzuo@gmail.com>
2020-09-30 11:38:02 +0800

    testify框架的使用

--- third-tool/testify/assert_test.go
+++ third-tool/testify/assert_test.go
@@ -2,8 +2,11 @@ package testify
 
 import (
 	"container/list"
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func Test_assert(t *testing.T) {
@@ -16,4 +19,12 @@ func Test_assert(t *testing.T) {
 	assertor.Nil(l.Front())
 
 	assert.True(t, true, "True is true")
+}
+
+// require没有返回值，而是终止当前测试
+func Test_require(t *testing.T) {
+	var name = "dazuo"
+	var age = 24
+	require.Equal(t, "dazuo", name, "they should be equal")
+	require.Equal(t, 24, age, "they should be equal")
 }
