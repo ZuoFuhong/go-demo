2e23db34d3da6635ce510ab50ac015c6edf59d6c
zfh <fuhongzuo@gmail.com>
2021-01-02 15:04:47 +0800

    CGO use static library

--- example/cgo/main.go
+++ example/cgo/main.go
@@ -1,14 +1,18 @@
 package main
 
-/*
-#include<stdio.h>
-
-void SayHello(const char* s) {
-    puts(s);
-}
-*/
+//#cgo CFLAGS: -I./number
+//#cgo LDFLAGS: -L${SRCDIR}/number -lnumber
+//
+//#include "number.h"
 import "C"
+import "fmt"
 
+// CGO使用静态库
+// $ cd ./number
+// $ gcc -c -o number.o number.c
+// $ ar rcs libnumber.a number.o
+//
+// 参考：https://chai2010.cn/advanced-go-programming-book/ch2-cgo/ch2-09-static-shared-lib.html
 func main() {
-	C.SayHello(C.CString("hello world"))
+	fmt.Println(C.number_add_mod(10, 5, 12))
 }
